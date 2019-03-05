71e0c27bc30e90f437a0b24a8f5c1326f313ae55
Jiří Škoda <skodaji4@fel.cvut.cz>
2019-03-05 09:08:21 +0100

    Initial commit with first two labs

---
+++ CV01/src/main/java/cz/cvut/fel/skodaj/b0b36pjv/cv01/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.fel.skodaj.b0b36pjv.cv01;
+
+/**
+ *
+ * @author skoda
+ */
+public class Main {
+    
+    /**
+     * Popis funkce main
+     * @param args Popis argumentu funkce
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+}
