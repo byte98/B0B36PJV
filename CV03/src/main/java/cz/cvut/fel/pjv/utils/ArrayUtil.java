a41345cbe0b6c16b3a7934e1752606d72858d20e
Jiří Škoda <skodaji4@fel.cvut.cz>
2019-03-06 09:46:29 +0100

    Lab 03

---
+++ CV03/src/main/java/cz/cvut/fel/pjv/utils/ArrayUtil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.fel.pjv.utils;
+
+/**
+ *
+ * @author Jiří Škoda <skodaji4@fel.cvut.cz>
+ */
+public final class ArrayUtil
+{
+    private ArrayUtil(){}
+    
+    public static void sort(Comparable[] array)
+    {
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            for (int j = 0; j < array.length - i - 1; j++)
+            {
+                if (array[j].compareTo(array[j+1]) < 0)
+                {
+                    Comparable tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
+    }
+}
